7c9f289b535c3eeb1da1f8c00b7bac33fd27c2f2
Corentin <corentincammas@gmail.com>
2020-05-24 16:53:28 +0200

    ajout de méthodes

--- Webservice/src/FileUtils/FileUtils.java
+++ Webservice/src/FileUtils/FileUtils.java
@@ -2,9 +2,11 @@ package FileUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -59,5 +61,61 @@ public class FileUtils {
 		
 	}
 	
+	public String getRDV (String nom) throws IOException {
+		String result="";
+		String line;
+		boolean foundClient=false;
+		File baseXML;
+		FileReader baseReader = new FileReader(baseXML);
+		BufferedReader baseBr = new BufferedReader(baseReader);
+		while((line = baseBr.readLine())!=null) {
+			if(line.contains(nom)) {
+				foundClient = true;
+			}
+			if(foundClient == true && line.contains("rdv")) {
+				result = line.substring(5, 23);
+			}
+		}
+		if (result == "") {
+			return "Désolé aucun rendez-vous trouvé !";
+		}
+		return result;
+	}
 	
+	public String getNom (String heure) throws IOException {
+		String result="";
+		String line="";
+		boolean foundClient=false;
+		boolean found = false;
+		File baseXML;
+		int compteur=0;
+		FileReader baseReader = new FileReader(baseXML);
+		BufferedReader baseBr = new BufferedReader(baseReader);
+		while((line = baseBr.readLine())!=null) {
+			compteur++;
+			if (line.contains(heure)) {
+				found = true;
+				baseBr.mark(compteur -4);
+				baseBr.reset();
+			}
+			if (found == true && (line.contains("prenom"))){
+				result = result + line.substring(8, (line.length()-8));
+			}
+			if (found==true && line.contains("nom")) {
+				result = result + line.substring(5, line.length()-5);
+			}
+		}
+		return result;
+	}
+	
+	public Collection getToday(String date) {
+		File baseXML;
+		String line;
+		int compteur=0;
+		FileReader baseReader = new FileReader(baseXML);
+		BufferedReader baseBr = new BufferedReader(baseReader);
+		while((line = baseBr.readLine())!=null) {
+			baseBr.lines().
+		}
+	}
 }
